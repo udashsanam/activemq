89b1beeaa8b399eb6b568d04eec8875ed0bc72f2
udashsanam <udashsanam@gmail.com>
2025-04-29 14:31:44 +0545

    add: minor change

--- activemq/src/main/java/com/learn/activemq/producer/JmsProducer.java
+++ activemq/src/main/java/com/learn/activemq/producer/JmsProducer.java
@@ -3,9 +3,10 @@ package com.learn.activemq.producer;
 import com.learn.activemq.model.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 @Slf4j
@@ -19,12 +20,7 @@ public class JmsProducer {
     public void sendMessage(Employee message){
         try{
             log.info("Attempting Send message to Topic: "+ topic);
-            jmsTemplate.convertAndSend("TEST", "hello");
-            jmsTemplate.convertAndSend("test", "hello");
-            jmsTemplate.convertAndSend("TEST", "hello");
-            jmsTemplate.convertAndSend("TEST", "hello");
-            jmsTemplate.convertAndSend("TEST", "hello");
-            jmsTemplate.convertAndSend("TEST", "hello");
+                    jmsTemplate.convertAndSend("TEST", String.format("{}", message));
         } catch(Exception e){
            log.error("Recieved Exception during send Message: ", e);
         }
